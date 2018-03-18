1ac8012ffbc9e419198b5acbe4bbbeb68dc71456
Mikk3lRo <mikk3lro@gmail.com>
2018-03-18 10:07:04 +0100

    Trying to automate versioning

--- increment_version.sh
+++ increment_version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-VERSION=git tag -l --sort -version:refname;
-echo "Old version: "$VERSION;
-NEW_VERSION=echo $VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}';
-echo "New version: "$NEW_VERSION;
+VERSION=$(git tag -l --sort -version:refname);
+echo "Old version: " $VERSION;
+NEW_VERSION=$(echo $VERSION | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}');
+echo "New version: " $NEW_VERSION;
