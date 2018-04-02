5e09747bb8dcf999d8b80b424c6090e018456865
Mikk3lRo <mikk3lro@gmail.com>
2018-04-02 18:04:36 +0200

    Completely refactor for PSR-3 compatibility

--- src/Logger.php
+++ src/Logger.php
@@ -1,109 +1,174 @@
 <?php
 namespace Mikk3lRo\atomix\io;
 
+use Psr\Log\LogLevel;
 use Mikk3lRo\atomix\system\DirConf;
+use Mikk3lRo\atomix\io\Formatters;
 
 class Logger {
+    use \Psr\Log\LoggerTrait;
+
     /**
      * Keeps track of the current indent level
-     * 
+     *
      * @var int
      */
-    static private $_indent = 0;
-    
+    private $_indent = 0;
+
     /**
      * Anything above this value will be ignored
-     * 
+     * - default WARNING (ie. debug, info, notice are ignored)
+     *
      * @var int
      */
-    static private $_max_log_level = 4;
-    
+    private $_max_log_level = 4;
+
     /**
      * Write log to cli / browser?
-     * 
+     *
      * @var bool
      */
-    static private $_output = true;
-    
+    private $_output = true;
+
     /**
      * The absolute path to the file we want to write to - or false for none
-     * 
-     * @var string 
+     *
+     * @var string
      */
-    static private $_log_filename = false;
-    
+    private $_log_filename = false;
+
     /**
      * Set the log filename
-     * 
+     *
      * @param mixed $filename A relative (to `DirConf::get('log')`) filename, or an absolute filename, or false to disable logging to file completely.
      */
-    static public function set_log_filename($filename) {
+    public function set_log_filename($filename) {
         if (!is_string($filename)) {
-            self::$_log_filename = false;
+            $this->_log_filename = false;
         } else if (substr($filename, 0, 1) !== '/') {
-            self::$_log_filename = DirConf::get('log') . '/' . $filename;
+            $this->_log_filename = DirConf::get('log') . '/' . $filename;
         } else {
-            self::$_log_filename = $filename;
+            $this->_log_filename = $filename;
         }
     }
-    
+
     /**
      * Get the log filename
      */
-    static public function get_log_filename() {
-        return self::$_log_filename;
-    }
-    
-    /**
-     * Sets the maximum log level that will be written / output by this class
-     * 
-     * @param int $level Default is 4 (which should be very verbose)
-     */
-    static public function set_max_log_level($level) {
-        self::$_max_log_level = intval($level);
-    }
-    
-    /**
-     * Get the maximum log level that will be written or output by this class
-     * 
-     * @return int
-     */
-    static public function get_max_log_level() {
-        return self::$_max_log_level;
+    public function get_log_filename() {
+        return $this->_log_filename;
     }
 
-        static public function set_output(bool $output) {
-        self::$_output = $output;
+    /**
+     * Sets the maximum log level that will be written / output by this class
+     *
+     * @param int $level Default is 4 = WARNING
+     */
+    public function set_max_log_level($level) {
+        $this->_max_log_level = self::numericLogLevel($level);
     }
-    
+
+    /**
+     * Get the maximum log level that will be written or output by this class
+     *
+     * @return int
+     */
+    public function get_max_log_level($as_int = false) {
+        if ($as_int === false) {
+            return self::stringLogLevel($this->_max_log_level);
+        }
+        return $this->_max_log_level;
+    }
+
+    public function set_output(bool $output) {
+        $this->_output = $output;
+    }
+
+    private static function numericLogLevel($loglevel) {
+        if (is_int($loglevel)) {
+            return $loglevel;
+        }
+        switch ($loglevel) {
+            case LogLevel::EMERGENCY:
+                return 0;
+            case LogLevel::ALERT:
+                return 1;
+            case LogLevel::CRITICAL:
+                return 2;
+            case LogLevel::ERROR:
+                return 3;
+            case LogLevel::WARNING:
+                return 4;
+            case LogLevel::NOTICE:
+                return 5;
+            case LogLevel::INFO:
+                return 6;
+            case LogLevel::DEBUG:
+            default:
+                return 7;
+        }
+    }
+
+    private static function stringLogLevel(int $loglevel) {
+        switch ($loglevel) {
+            case 0:
+                return LogLevel::EMERGENCY;
+            case 1:
+                return LogLevel::ALERT;
+            case 2:
+                return LogLevel::CRITICAL;
+            case 3:
+                return LogLevel::ERROR;
+            case 4:
+                return LogLevel::WARNING;
+            case 5:
+                return LogLevel::NOTICE;
+            case 6:
+                return LogLevel::INFO;
+            case 7:
+            default:
+                return LogLevel::DEBUG;
+        }
+    }
+
+
     /**
      * Appends to a flat logfile (if enabled), and outputs to browser / cli (if enabled)
-     *  
-     * @param mixed     $string_or_array    What to write
-     * @param int       $log_level          Higher levels are hidden when output verbosity is reduced
+     *
+     * @param string $level One of the constants from Psr\Log\LogLevel
+     * @param string $message What to log (normally a string)
+     * @param array  $context An array of substitutions to make in the passed string
+     *
+     * @return void
      */
-    static function write($what, $log_level = 0) {
-        if (self::$_max_log_level < $log_level) {
+    public function log($level, $message, array $context = array()) {
+        $level_int = self::numericLogLevel($level);
+        if ($this->_max_log_level < $level_int) {
             //Ignore it completely
             return;
         }
-        
-        //Convert anything that is not a string
-        if (!is_string($what)) {
-            $what = var_export($what, true);
-        }
-        
-        $log_time = '[' . date('Y-m-d H:i:s') . ']';
-        $log_ident = '[' . str_pad(getmypid(), 6, ' ', STR_PAD_LEFT) . ']';
-        $log_indent = self::indent();
-        $log_string = str_replace("\n", "\n" . $log_indent . str_repeat(' ', 30), trim($what));
 
-        $output = $log_time . $log_ident . ' ' . $log_indent . $log_string . "\n";
-        
-        if (is_string(self::$_log_filename)) {
-            file_put_contents(self::$_log_filename, $output, FILE_APPEND);
+        //Convert anything that is not a string
+        if (!is_string($message)) {
+            $message = var_export($message, true);
         }
-        if (self::$_output) {
+
+        if (!empty($context)) {
+            $message = Formatters::replaceTags($message, $context);
+        }
+
+        $log_time = '[' . date('Y-m-d H:i:s') . ']';
+        $log_ident = str_pad('[' . getmypid() . ']', 8, ' ', STR_PAD_LEFT);
+        $log_level_str = str_pad('[' . $level . ']', 11, ' ', STR_PAD_LEFT);
+        $log_indent = $this->indent();
+        $log_string = str_replace("\n", "\n" . $log_indent . str_repeat(' ', 41), trim($message));
+
+        $output = $log_time . $log_ident . $log_level_str . ' ' . $log_indent . $log_string . "\n";
+
+        if (is_string($this->_log_filename)) {
+            file_put_contents($this->_log_filename, $output, FILE_APPEND);
+        }
+        if ($this->_output) {
             echo $output;
         }
     }
@@ -111,25 +176,25 @@ class Logger {
     /**
      * Increases the log indentation by 4 spaces.
      */
-    static function indent_increase() {
-        self::$_indent += 4;
+    public function indent_increase() {
+        $this->_indent += 4;
     }
 
     /**
      * Decreases the log indentation by 4 spaces.
-     * 
+     *
      * @global int $log_indent
      */
-    static function indent_decrease() {
-        self::$_indent -= 4;
+    public function indent_decrease() {
+        $this->_indent -= 4;
     }
 
     /**
      * Gets the current log indent as a string - ready to prepend each line.
-     * 
+     *
      * @return string
      */
-    static private function indent() {
-        return str_repeat(' ', self::$_indent);
+    private function indent() {
+        return str_repeat(' ', $this->_indent);
     }
 }
--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -4,108 +4,189 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 use Mikk3lRo\atomix\io\Logger;
+use Psr\Log\LogLevel;
 use Mikk3lRo\atomix\system\DirConf;
 
 DirConf::define('log', '/tmp/testlogs');
 
 final class LoggerTest extends TestCase
 {
-    public function testCanWrite() {
-        $this->expectOutputRegex('#^\[[^\]]+\]\[[^\]]+\]\sAnd it was written...$#');
-        Logger::write('And it was written...');
+    private $log_pcre_prefix = '(\[[^\]]+\]\s*){3}\s';
+    public function testCanInstantiate() {
+        $logger = new Logger();
+        $this->assertInstanceOf(Logger::class, $logger);
+        return $logger;
     }
-    public function testCanWriteNonString1() {
-        $this->expectOutputRegex('#^\[[^\]]+\]\[[^\]]+\]\sfalse$#');
-        Logger::write(false);
+
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testCanWrite(Logger $logger) {
+        $this->expectOutputRegex('#^' . $this->log_pcre_prefix . 'And it was written...$#');
+        $logger->error('And it was written...');
     }
-    public function testCanWriteNonString2() {
-        $this->expectOutputRegex('#^\[[^\]]+\]\[[^\]]+\] array \(#');
-        Logger::write(array(1, 2, 3));
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testCanWriteNonString1(Logger $logger) {
+        $this->expectOutputRegex('#^' . $this->log_pcre_prefix . 'false$#');
+        $logger->error(false);
     }
-    public function testIgnoresHighLogLevels() {
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testCanWriteNonString2(Logger $logger) {
+        $this->expectOutputRegex('#^' . $this->log_pcre_prefix . 'array \(#');
+        $logger->error(array(1, 2, 3));
+    }
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testCanReplaceContext(Logger $logger) {
+        $this->expectOutputRegex('#^' . $this->log_pcre_prefix . 'I have an apple and a banana#');
+        $logger->error('I have an {fruit1} and a {fruit2}', array(
+            'fruit1' => 'apple',
+            'fruit2' => 'banana'
+        ));
+    }
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testRespectsLogLevels1(Logger $logger) {
+        $logger->set_max_log_level(LogLevel::WARNING);
         $this->expectOutputString('');
-        Logger::write('And it was written...', 999);
+        $logger->debug('And it was written...');
     }
-    public function testCanSetLogLevel() {
-        Logger::set_max_log_level(1000);
-        $this->expectOutputRegex('#^\[[^\]]+\]\[[^\]]+\]\sAnd it was written...$#');
-        Logger::write('And it was written...', 999);
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testRespectsLogLevels2(Logger $logger) {
+        $logger->set_max_log_level(LogLevel::DEBUG);
+        $this->expectOutputRegex('#^' . $this->log_pcre_prefix . 'And it was written...$#');
+        $logger->debug('And it was written...');
     }
-    public function testCanGetLogLevel() {
-        Logger::get_max_log_level();
-        $this->assertEquals(1000, Logger::get_max_log_level());
-        Logger::set_max_log_level(4);
-        $this->assertEquals(4, Logger::get_max_log_level());
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testCanSetAndGetAllLogLevels(Logger $logger) {
+        $logger->set_max_log_level(LogLevel::DEBUG);
+        $this->assertEquals(LogLevel::DEBUG, $logger->get_max_log_level());
+
+        $logger->set_max_log_level(LogLevel::INFO);
+        $this->assertEquals(LogLevel::INFO, $logger->get_max_log_level());
+
+        $logger->set_max_log_level(LogLevel::NOTICE);
+        $this->assertEquals(LogLevel::NOTICE, $logger->get_max_log_level());
+
+        $logger->set_max_log_level(LogLevel::WARNING);
+        $this->assertEquals(LogLevel::WARNING, $logger->get_max_log_level());
+
+        $logger->set_max_log_level(LogLevel::ERROR);
+        $this->assertEquals(LogLevel::ERROR, $logger->get_max_log_level());
+
+        $logger->set_max_log_level(LogLevel::CRITICAL);
+        $this->assertEquals(LogLevel::CRITICAL, $logger->get_max_log_level());
+
+        $logger->set_max_log_level(LogLevel::ALERT);
+        $this->assertEquals(LogLevel::ALERT, $logger->get_max_log_level());
+
+        $logger->set_max_log_level(LogLevel::EMERGENCY);
+        $this->assertEquals(LogLevel::EMERGENCY, $logger->get_max_log_level());
     }
-    public function testRespectsIndent1() {
-        Logger::indent_increase();
-        $this->expectOutputRegex('#^\[[^\]]+\]\[[^\]]+\]\s    And it was written...$#');
-        Logger::write('And it was written...');
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testCanSetNumericLogLevel(Logger $logger) {
+        $logger->set_max_log_level(4);
+        $this->assertEquals(4, $logger->get_max_log_level(true));
     }
-    public function testRespectsIndent2() {
-        Logger::indent_increase();
-        $this->expectOutputRegex('#^\[[^\]]+\]\[[^\]]+\]\s        And it was written...$#');
-        Logger::write('And it was written...');
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testRespectsIndent1(Logger $logger) {
+        $logger->indent_increase();
+        $this->expectOutputRegex('#^' . $this->log_pcre_prefix . '    And it was written...$#');
+        $logger->error('And it was written...');
     }
-    public function testRespectsIndent3() {
-        Logger::indent_decrease();
-        $this->expectOutputRegex('#^\[[^\]]+\]\[[^\]]+\]\s    And it was written...$#');
-        Logger::write('And it was written...');
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testRespectsIndent2(Logger $logger) {
+        $logger->indent_increase();
+        $this->expectOutputRegex('#^' . $this->log_pcre_prefix . '        And it was written...$#');
+        $logger->error('And it was written...');
     }
-    public function testRespectsIndent4() {
-        Logger::indent_decrease();
-        $this->expectOutputRegex('#^\[[^\]]+\]\[[^\]]+\]\sAnd it was written...$#');
-        Logger::write('And it was written...');
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testRespectsIndent3(Logger $logger) {
+        $logger->indent_decrease();
+        $this->expectOutputRegex('#^' . $this->log_pcre_prefix . '    And it was written...$#');
+        $logger->error('And it was written...');
     }
-    
-    public function testWriteToAbsFile() {
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testRespectsIndent4(Logger $logger) {
+        $logger->indent_decrease();
+        $this->expectOutputRegex('#^' . $this->log_pcre_prefix . 'And it was written...$#');
+        $logger->error('And it was written...');
+    }
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testWriteToAbsFile(Logger $logger) {
         $tmpname = '/tmp/logtest1';
         if (file_exists($tmpname)) {
             unlink($tmpname);
         }
-        
-        Logger::set_log_filename($tmpname);
-        Logger::set_output(false);
-        Logger::write('And it was written...');
-        
+
+        $logger->set_log_filename($tmpname);
+        $logger->set_output(false);
+        $logger->error('And it was written...');
+
         $this->assertFileExists($tmpname);
-        
-        $this->assertRegExp('#^\[[^\]]+\]\[[^\]]+\]\sAnd it was written...$#', file_get_contents($tmpname));
-        
+
+        $this->assertRegExp('#^' . $this->log_pcre_prefix . 'And it was written...$#', file_get_contents($tmpname));
+
         unlink($tmpname);
     }
-    
-    public function testWriteToRelFile() {
-        Logger::set_log_filename('logtest2');
-        $logfile_actual = Logger::get_log_filename();
-        
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testWriteToRelFile(Logger $logger) {
+        $logger->set_log_filename('logtest2');
+        $logfile_actual = $logger->get_log_filename();
+
         $this->assertEquals('/tmp/testlogs/logtest2', $logfile_actual);
-        
+
         if (file_exists($logfile_actual)) {
             unlink($logfile_actual);
         }
-        
-        Logger::set_output(false);
-        Logger::write('And it was written...');
-        
+
+        $logger->set_output(false);
+        $logger->error('And it was written...');
+
         $this->assertFileExists($logfile_actual);
-        
-        $this->assertRegExp('#^\[[^\]]+\]\[[^\]]+\]\sAnd it was written...$#', file_get_contents($logfile_actual));
-        
+
+        $this->assertRegExp('#^' . $this->log_pcre_prefix . 'And it was written...$#', file_get_contents($logfile_actual));
+
         unlink($logfile_actual);
     }
-    
-    public function testFailsToSetNonStringFilename() {
-        Logger::set_log_filename('logtest3');
-        $this->assertEquals('/tmp/testlogs/logtest3', Logger::get_log_filename());
-        
-        Logger::set_log_filename(true);
-        $this->assertEquals(false, Logger::get_log_filename());
-        
-        Logger::set_log_filename('logtest4');
-        $this->assertEquals('/tmp/testlogs/logtest4', Logger::get_log_filename());
-        
-        Logger::set_log_filename(array('this should fail', 'SO badly'));
-        $this->assertEquals(false, Logger::get_log_filename());
+    /**
+     * @depends testCanInstantiate
+     */
+    public function testFailsToSetNonStringFilename(Logger $logger) {
+        $logger->set_log_filename('logtest3');
+        $this->assertEquals('/tmp/testlogs/logtest3', $logger->get_log_filename());
+
+        $logger->set_log_filename(true);
+        $this->assertEquals(false, $logger->get_log_filename());
+
+        $logger->set_log_filename('logtest4');
+        $this->assertEquals('/tmp/testlogs/logtest4', $logger->get_log_filename());
+
+        $logger->set_log_filename(array('this should fail', 'SO badly'));
+        $this->assertEquals(false, $logger->get_log_filename());
     }
 }
